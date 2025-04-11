c9341eb208ba4f3d7359b45aa61d019d6bcbada9
Amit H Bhagat <amitbhagat.h@gmail.com>
2025-04-11 19:34:13 +0530

    api url corrected

--- src/envConfig.js
+++ src/envConfig.js
@@ -1,6 +1,6 @@
 const ServerBase = import.meta.env.DEV
-  ? "hhttps://1d7e-2401-4900-1c21-1f7f-9c12-8ed7-dc78-a3e.ngrok-free.app"
-  : "hhttps://1d7e-2401-4900-1c21-1f7f-9c12-8ed7-dc78-a3e.ngrok-free.app";
+  ? "https://1d7e-2401-4900-1c21-1f7f-9c12-8ed7-dc78-a3e.ngrok-free.app"
+  : "https://1d7e-2401-4900-1c21-1f7f-9c12-8ed7-dc78-a3e.ngrok-free.app";
 // : "http://13.234.32.219:3030";
 
 const EnvConfig = {
