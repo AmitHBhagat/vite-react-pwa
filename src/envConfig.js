e254b39a212891038b3e0c7c9974986ec399ec9e
Amit H Bhagat <amitbhagat.h@gmail.com>
2025-04-15 17:38:41 +0530

    api url updated

--- src/envConfig.js
+++ src/envConfig.js
@@ -1,6 +1,7 @@
 const ServerBase = import.meta.env.DEV
   ? "http://localhost:8000"
-  : "http://13.234.32.219:3030";
+  : "https://a5ad-2401-4900-1c94-7e03-c59-a076-65f6-26a6.ngrok-free.app";
+// : "http://13.234.32.219:3030";
 
 const EnvConfig = {
   ApiBase: `${ServerBase}/api/v1`,
